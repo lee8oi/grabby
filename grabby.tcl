1f5e3fe41dc16c8f997966583e22221e2ce5be62
Lee Forest <lee8oi@gmail.com>
2011-10-29 11:37:42 -0400

    First commit

---
+++ grabby.tcl
@@ -0,0 +1,63 @@
+##################################################################################
+# Copyright ©2011 lee8oi@gmail.com
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# http://www.gnu.org/licenses/
+############################################
+#uncomment to enable local copy of http.tcl
+source "http.tcl"
+package require http 2.5.3
+############################################
+package require http
+package require tls
+::http::register https 443 [list ::tls::socket -require 0 -request 1]
+set urls {
+	"http://www.google.com"
+	"http://forum.egghelp.org/viewtopic.php?t=16819"
+	"http://fishki.net/comment.php?id=20554"
+	"http://www.nicovideo.jp/video_top"
+	"http://www.duzheer.com/youqingwenzhang/"
+	"http://plus.google.com"
+}
+puts "package version: [package provide http]"
+puts "running grabby"
+proc grabby {url} {
+	catch {set http [::http::geturl $url -timeout 60000]} error
+	if {[info exists http]} {
+		if { [::http::status $http] == "timeout" } {
+			puts "Oops timed out."
+			return 0
+		}
+		upvar #0 $http state
+		array set meta $state(meta)
+		foreach {name value} $state(meta) {
+			if {[regexp -nocase ^location$ $name]} {
+				# Handle URL redirects
+				puts "redirect found: $value"
+				grabby $value
+				return 0
+			}
+		}
+		puts "State url: $state(url)"
+		puts "State type: $state(type)"
+		puts "Document encoding: $state(charset)"
+		::http::cleanup $http
+		return 1
+	} else {
+		puts "no http data found."
+		return 0
+	}
+	
+}
+foreach url $urls {
+	puts "infor for $url: [grabby $url]"
+}
+
